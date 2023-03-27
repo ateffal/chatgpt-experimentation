28c74eb7f1b3d8999dbb3cd200d4d84aedd67ce8
Amine TEFFAL <a.teffal@gmail.com>
2023-03-27 22:09:56 +0000

    added tabs

--- app.py
+++ app.py
@@ -7,33 +7,6 @@ from PIL import Image
 
 openai.api_key = st.secrets["api_key"] 
 
-st.set_page_config(
-    page_title="Omdena Lublin Chapter: Financial Chatbot",
-    page_icon="https://i.ibb.co/ZYG59cp/omdena-transparent.png"
-)
-
-
-st.image("https://i.ibb.co/H2z983v/Omdena-Lublin-Chapter.jpg", caption=None,
-         width=300, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
-
-st.header("AI Tool for Linguistic and Psychometric Assessemnt")
-
-st.caption("Disclaimer: The Content is for informational purposes only, you should not construe any such information or other material as legal, tax, investment, financial, or other advice.")
-
-st.markdown(
-    "Wondering how to  improve your soft skills ? Here is the solution!")
-
-st.markdown("You can ask it questions like:")
-
-st.markdown(
-    "***How to have clear communication ?*** ***What is active listenning ?*** ***How to be a good listener ?***")
-
-
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
 
 
 # def query(payload):
@@ -85,17 +58,66 @@ def model_response(question):
     return response['choices'][0]['text']
 
 
-user_input = get_text()
+
+st.set_page_config(
+    page_title="Omdena Lublin Chapter: Financial Chatbot",
+    page_icon="https://i.ibb.co/ZYG59cp/omdena-transparent.png"
+)
 
 
-if user_input:
-    output = model_response(user_input)
-    st.session_state.past.append(user_input)
-    # st.session_state.generated.append(output["generated_text"])
-    st.session_state.generated.append(output)
+st.image("https://i.ibb.co/H2z983v/Omdena-Lublin-Chapter.jpg", caption=None,
+         width=300, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i],
-                is_user=True, key=str(i) + '_user')
+st.header("AI Tool for Linguistic and Psychometric Assessemnt")
+
+st.caption("Disclaimer: This application is for showcasing code pusued to Github. It is not a replacement for task-3 and task-4.")
+
+tab1, tab2, tab3 = st.tabs(["Ask question", "Summarization", "Classification"])
+
+with tab1:
+
+    st.markdown(
+        "Wondering how to  improve your soft skills ? Here is the solution!")
+
+    st.markdown("You can ask it questions like:")
+
+    st.markdown(
+        "***How to have clear communication ?*** ***What is active listenning ?*** ***How to be a good listener ?***")
+
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+
+
+    user_input = get_text()
+
+
+    if user_input:
+        output = model_response(user_input)
+        st.session_state.past.append(user_input)
+        # st.session_state.generated.append(output["generated_text"])
+        st.session_state.generated.append(output)
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i],
+                    is_user=True, key=str(i) + '_user')
+
+with tab2:
+
+    st.header(
+        "Text Summarizationunder construction....")
+
+    title = st.text_input('Text to summarize :')
+    
+
+
+with tab3:
+
+    st.header(
+        "Classification under construction....")
+    title = st.text_input('Text classify :')
