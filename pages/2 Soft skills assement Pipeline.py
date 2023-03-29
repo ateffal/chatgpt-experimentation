288f856b38dbbe413f085e66bdce9fff239d4468
Amine TEFFAL <a.teffal@gmail.com>
2023-03-29 22:21:11 +0000

    update

--- pages/2 Soft skills assement Pipeline.py
+++ pages/2 Soft skills assement Pipeline.py
@@ -69,8 +69,8 @@ with tab1:
 
                 transcription, trans_time_ = transcript_file(video)
 
-                st.text('Execution time in seconds : ', trans_time_)
-                st.text('[', trans_time_/60, ' minutes.', ']')
+                st.text('Execution time in seconds : ' + str(trans_time_))
+                st.text('[' + str(int(trans_time_/60)) +  ' minutes.', ']')
 
                 st.text_area("", value= transcription, height=500)
 
