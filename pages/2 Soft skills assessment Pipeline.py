580dc9ecab1ece23c9bbd48591e711b94aab22c7
Amine TEFFAL <a.teffal@gmail.com>
2023-04-01 17:18:43 +0000

    update

--- pages/2 Soft skills assessment Pipeline.py
+++ pages/2 Soft skills assessment Pipeline.py
@@ -119,29 +119,29 @@ with tab1:
 
                 st.text_area("", value= transcription, height=800)
 
-                audio_ = convert_to_wav(video, "")
+                # audio_ = convert_to_wav(video, "")
 
-                dz = diarize_audio(audio_)
+                # dz = diarize_audio(audio_)
 
-                with open("diarization.txt", "w") as text_file:
-                    text_file.write(str(dz))
+                # with open("diarization.txt", "w") as text_file:
+                #     text_file.write(str(dz))
 
-                turns = open('diarization.txt').read().splitlines()
+                # turns = open('diarization.txt').read().splitlines()
 
-                starts = []
-                ends = []
-                speakers = []
+                # starts = []
+                # ends = []
+                # speakers = []
 
-                for turn in turns:
-                    t1, t2 = re.findall('[0-9]+:[0-9]+:[0-9]+', string=turn)
-                    speaker = re.findall('SPEAKER_[0-9][0-9]', string=turn)
-                    starts.append(t1)
-                    ends.append(t2)
-                    speakers.append(speaker[0])
+                # for turn in turns:
+                #     t1, t2 = re.findall('[0-9]+:[0-9]+:[0-9]+', string=turn)
+                #     speaker = re.findall('SPEAKER_[0-9][0-9]', string=turn)
+                #     starts.append(t1)
+                #     ends.append(t2)
+                #     speakers.append(speaker[0])
 
-                df = pd.DataFrame({'start' : starts, 'end' : ends , 'speaker': speakers})
+                # df = pd.DataFrame({'start' : starts, 'end' : ends , 'speaker': speakers})
 
-                st.dataframe(data=df)
+                # st.dataframe(data=df)
 
                 
 
